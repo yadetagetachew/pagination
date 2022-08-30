d39d99e99bf34b63c77792180a8d3dac8311100b
Yadeta Getachew <86324546+yadetagetachew@users.noreply.github.com>
2022-08-30 14:10:25 -0700

    starter code added

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+
+/*
+Create the `showPage` function
+This function will create and insert/append the elements needed to display a "page" of nine students
+*/
+
+
+
+/*
+Create the `addPagination` function
+This function will create and insert/append the elements needed for the pagination buttons
+*/
+
+
+
+// Call functions
